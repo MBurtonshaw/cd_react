b6d0b5308735888c926b622b76d4ce2258290eb2
Matt <MBurtonshaw@gmail.com>
2024-03-26 22:48:08 -0400

    Header basically done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import { React } from 'react';
+import Header from './Header';
+import Carousel from './Carousel';
 
 export default function Home(props) {
     return (
         <div id='home_div' className='mt-3'>
-            <h1>Home</h1>
-            <div className='row align-items-start mt-5'>
-                <a className='col' href='/about'>About</a>
-                <a className='col' href='/contact'>Contact</a>
-                <a className='col' href='/gallery'>Gallery</a>
-                <a className='col' href='/menu'>Menu</a>
+            <Header />
+            <Carousel />
+            <div>
+                <h1 className='font-color-2'>Prologue</h1>
+                <p className='font-color-1'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neightborhood of Cleveland.</p>
+                <p className='font-color-1'>Tues - Sun <span className='font-color-2'>4pm - 11pm</span></p>
             </div>
         </div>
     );
