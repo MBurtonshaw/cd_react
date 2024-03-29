60f2164649c56f7ac93accfbd42bed4c774e2e51
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-03-28 22:08:47 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Gallery from './components/Gallery';
-import Menu from './components/Menu';
+import Home from './components/home/Home';
+import About from './components/home/About';
+import Contact from './components/home/Contact';
+import Gallery from './components/home/Gallery';
+import Menu from './components/home/Menu';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/contact' element={<Contact />} />
-    <Route path='/gallery' element={<Gallery />} />
-    <Route path='/menu' element={<Menu />} />
-    </Routes>
-    </BrowserRouter>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/menu' element={<Menu />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
