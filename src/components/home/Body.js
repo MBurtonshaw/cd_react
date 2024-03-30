42e8b44bfaf9d66c4434bb3485adf56918309561
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-03-30 16:02:25 -0400

    Add files via upload

--- src/components/home/Body.js
+++ src/components/home/Body.js
@@ -4,20 +4,30 @@ export default function Body(props) {
     return (
         <div id='body_div' className='w-100'>
             <div className='row my-4'>
-            <div id='eat_drink' className='col'>
-                <img width='265' height='auto' src='../img/eat_drink.png' />
-                <div className='w-100 mt-4'>
-                <button className=' row px-5 mx-auto' type='button'><a className='nonchalant' href='/menu'>View Menu Book</a></button>
+                <div id='body_paragraphs' className='col'>
+                    <img width='265' height='auto' src='../img/eat_drink.png' />
+                    <p className='body_text'>A journey through the Tales of Life & Death; our debut menu.</p>
+                    <div className='w-100 mt-4'>
+                        <div className='button_div'>
+                            <a className='nonchalant black_link' href='/menu'><h4 className='button_h4'>VIEW MENU BOOK</h4></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-                <img className='col' width='750' height='500' src='../img/MenuImage_1.png' />
+                <a href='/menu' className='col'>
+                    <img width='100%' src='../img/MenuImage_1.png' />
+                </a>
             </div>
             <div className='row my-4'>
-                <img className='col' width='750' height='500' src='../img/GalleryImage_1.png' />
-                <div id='body_gallery' className='col'>
+                <a className='col' width='auto' href='/gallery'>
+                    <img width='100%' src='../img/GalleryImage_1.png' />
+                </a>
+                <div id='body_paragraphs' className='col'>
                     <img width='300' src='../img/gallery.png' />
+                    <p className='body_text'>A look inside Cloak & Dagger. All videos and photos captured by Josh Dobay.</p>
                     <div className='w-100 mt-4'>
-                    <button className='row px-5 mx-auto' type='button'><a className='nonchalant' href='/gallery'>View Gallery</a></button>
+                        <div className='button_div'>
+                            <a className='nonchalant black_link' href='/gallery'><h4 className='button_h4'>VIEW GALLERY</h4></a>
+                        </div>
                     </div>
                 </div>
             </div>
