b6ae525872ebeeaaa67a057318d750481c763926
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-03-30 15:47:27 -0400

    Homepage done?

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,17 @@
 import { React } from 'react';
+import Newsletter from '../components/Newsletter';
 
 export default function Footer(props) {
     return (
-        <div id='footer_div' className='w-100 py-2 row'>
-            <div className='col'>
+        <div id='footer_div' className='w-100 mx-auto py-1 row'>
+            <div className='col m-auto'>
                 <img width='60' className='little_right mx-4 my-3' src='../img/CD_Facebook.png' />
                 <img width='60' className='little_right mx-4 my-3' src='../img/CD_Instagram.png' />
             </div>
-            <div id='footer_two' className='col-9 mt-5'>
-                <div className='inner-footer row'>
-                <p className='font-color-1 newsletter_text col'>sign up for our newsletter</p>
-                <input className='newsletter_input col' placeholder='Sign up for our newsletter'></input>
-                <button className='newsletter_button col-1' type='button'>Join</button>
-                </div>
-                
+
+            <div className='col'>
+            <Newsletter />
             </div>
-        </div>
+  </div>
     );
 }
--- components/home/Body.js
+++ components/home/Body.js
@@ -9,24 +9,24 @@ export default function Body(props) {
                     <p className='body_text'>A journey through the Tales of Life & Death; our debut menu.</p>
                     <div className='w-100 mt-4'>
                         <div className='button_div'>
-                            <h4 className='button_h4'>
-                                VIEW MENU BOOK
-                            </h4>
+                            <a className='nonchalant black_link' href='/menu'><h4 className='button_h4'>VIEW MENU BOOK</h4></a>
                         </div>
                     </div>
                 </div>
-                <img className='col' width='auto' src='../img/MenuImage_1.png' />
+                <a href='/menu' className='col'>
+                    <img width='100%' src='../img/MenuImage_1.png' />
+                </a>
             </div>
             <div className='row my-4'>
-                <img className='col' width='auto' src='../img/GalleryImage_1.png' />
+                <a className='col' width='auto' href='/gallery'>
+                    <img width='100%' src='../img/GalleryImage_1.png' />
+                </a>
                 <div id='body_paragraphs' className='col'>
                     <img width='300' src='../img/gallery.png' />
                     <p className='body_text'>A look inside Cloak & Dagger. All videos and photos captured by Josh Dobay.</p>
                     <div className='w-100 mt-4'>
                         <div className='button_div'>
-                            <h4 className='button_h4'>
-                                VIEW GALLERY
-                            </h4>
+                            <a className='nonchalant black_link' href='/gallery'><h4 className='button_h4'>VIEW GALLERY</h4></a>
                         </div>
                     </div>
                 </div>
