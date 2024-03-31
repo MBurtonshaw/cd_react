e4f57ec7f8f7ff7b800e111251f5871946b9f996
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-03-31 19:32:54 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,50 @@
 import { React } from 'react';
 
 export default function Header(props) {
+    if (window.innerWidth < 992) {
+        return (
+            <div id='header_div'>
+                <div className='row my-3'>
+                    <div className='col-7 my-1 mx-3'>
+                        <a href='/'><img className='' width='100%' src='../img/CD_Logo_Horizontal.png' /></a>
+                    </div>
+                    <div className='col-4'>
+                        <div className="dropdown">
+
+                            <img width='50%' height='50%' className='m-auto dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false" src='../img/mobile_menu_icon.png' />
+
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div className='address_bar'>
+                    <p className='w-75 m-auto px-3'>
+                        2399 W. 11TH ST. CLEVELAND, OH 44113 ·
+                        (216) 795-5657
+                    </p>
+                </div>
+            </div>
+        );
+    }
     return (
         <div id='header_div'>
-            <div className='row align-items-start my-3'>
+            <div className='row my-2 w-100 mx-auto'>
                 <div className='col'>
-                    <a href='/'><img className='nav_img' src='../img/CD_Logo_Horizontal.png' /></a>
+                    <a className='' href='/'><img className='nav_img p-1' src='../img/CD_Logo_Horizontal.png' /></a>
                 </div>
                 <div className='col'>
-                    <div className='d-flex justify-content-around nav_bar'>
-                        <a className='mx-auto nav_item nonchalant font-color-1' href='/menu'>MENU</a>
-                        <a className='mx-auto nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
-                        <a className='mx-auto nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
-                        <a className='mx-auto nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
-                        <a className='mx-auto nav_item nonchalant font-color-1' href='/takeout'>TAKEOUT</a>
+                    <div className='d-flex justify-content-end nav_bar'>
+                        <a className='px-2 nav_item nonchalant font-color-1' href='/menu'>MENU</a>
+                        <a className='px-2 nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
+                        <a className='px-2 nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
+                        <a className='px-2 nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
+                        <a className='px-2 nav_item nonchalant font-color-1' href='/takeout'>TAKEOUT</a>
                     </div>
                 </div>
             </div>
@@ -25,4 +56,5 @@ export default function Header(props) {
             </div>
         </div>
     );
+
 }
