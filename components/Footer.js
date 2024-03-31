696380866e4104c0f2245737a92cf3c7cbcc4db2
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-03-31 19:31:59 -0400

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,29 @@ import Newsletter from '../components/Newsletter';
 
 export default function Footer(props) {
     return (
-        <div id='footer_div' className='w-100 mx-auto py-1 row'>
-            <div className='col m-auto'>
-                <img width='60' className='little_right mx-4 my-3' src='../img/CD_Facebook.png' />
-                <img width='60' className='little_right mx-4 my-3' src='../img/CD_Instagram.png' />
-            </div>
+        <div id='' className=''>
+            <div id="seven">
+                <p id="seven_text">
+                    Sign up for our newsletter
+                </p>
+                <div id="seven_socials_div">
+                    <a
+                        id="seven_facebook_a"
+                        target="_blank"
+                        href="https://facebook.com/cloakanddaggercle"
+                    >
+                        <img id="seven_facebook" src="img/CD_Facebook.png" />
+                    </a>
 
-            <div className='col'>
-            <Newsletter />
+                    <a
+                        id="seven_instagram_a"
+                        target="_blank"
+                        href="https://instagram.com/cloakanddaggercle"
+                    >
+                        <img id="seven_instagram" src="img/CD_Instagram.png" />
+                    </a>
+                </div>
+                <div className="klaviyo-form-RBSHP6"></div>
             </div>
   </div>
     );
---
+++ components/Loading.js
@@ -0,0 +1,9 @@
+import { React } from 'react';
+
+export default function Loading(props) {
+    return (
+        <div id='loading_div' className=''>
+            <h1>Loading...</h1>
+        </div>
+    );
+}
