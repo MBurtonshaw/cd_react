b0bc98e651d21374c0a961c2108a5db83c85043e
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-04-01 22:05:20 -0400

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,111 @@
 import { React } from 'react';
-import Newsletter from '../components/Newsletter';
 
-export default function Footer(props) {
+export default function Footer() {
+    if (window.innerWidth < 1500) {
+        if (window.innerWidth < 1200) {
+            if (window.innerWidth < 900) {
+                if (window.innerWidth < 768) {
+                    if (window.innerWidth < 575) {
+                        //smallest
+                        return (
+                            <div id='' className=''>
+                                <div id="seven">
+                                    <p id="seven_text">
+                                        Sign up for our newsletter
+                                    </p>
+                                    <div id="seven_socials_div">
+                                        <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                                            <img id="seven_facebook" className='socials_5' src="img/CD_Facebook.png" />
+                                        </a>
+                                        <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                                            <img id="seven_instagram" className='socials_5' src="img/CD_Instagram.png" />
+                                        </a>
+                                    </div>
+                                    <div className="klaviyo-form-RBSHP6"></div>
+                                </div>
+                            </div>
+                        );
+                    }
+                    //smaller
+                    return (
+                        <div id='' className=''>
+                            <div id="seven">
+                                <p id="seven_text">
+                                    Sign up for our newsletter
+                                </p>
+                                <div id="seven_socials_div">
+                                    <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                                        <img id="seven_facebook" className='socials_4' src="img/CD_Facebook.png" />
+                                    </a>
+                                    <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                                        <img id="seven_instagram" className='socials_4' src="img/CD_Instagram.png" />
+                                    </a>
+                                </div>
+                                <div className="klaviyo-form-RBSHP6"></div>
+                            </div>
+                        </div>
+                    );
+                }
+                //small
+                return (
+                    <div id='' className=''>
+                        <div id="seven">
+                            <p id="seven_text">
+                                Sign up for our newsletter
+                            </p>
+                            <div id="seven_socials_div">
+                                <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                                    <img id="seven_facebook" className='socials_3' src="img/CD_Facebook.png" />
+                                </a>
+                                <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                                    <img id="seven_instagram" className='socials_3' src="img/CD_Instagram.png" />
+                                </a>
+                            </div>
+                            <div className="klaviyo-form-RBSHP6"></div>
+                        </div>
+                    </div>
+                );
+            }
+            //medium
+            return (
+                <div id='' className=''>
+                    <div id="seven">
+                        <p id="seven_text">
+                            Sign up for our newsletter
+                        </p>
+                        <div id="seven_socials_div">
+                            <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                                <img id="seven_facebook" className='socials_3' src="img/CD_Facebook.png" />
+                            </a>
+                            <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                                <img id="seven_instagram" className='socials_3' src="img/CD_Instagram.png" />
+                            </a>
+                        </div>
+                        <div className="klaviyo-form-RBSHP6"></div>
+                    </div>
+                </div>
+            );
+        }
+        //large
+        return (
+            <div id='' className=''>
+                <div id="seven">
+                    <p id="seven_text">
+                        Sign up for our newsletter
+                    </p>
+                    <div id="seven_socials_div">
+                        <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                            <img id="seven_facebook" className='socials_2' src="img/CD_Facebook.png" />
+                        </a>
+                        <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                            <img id="seven_instagram" className='socials_2' src="img/CD_Instagram.png" />
+                        </a>
+                    </div>
+                    <div className="klaviyo-form-RBSHP6"></div>
+                </div>
+            </div>
+        );
+    }
     return (
         <div id='' className=''>
             <div id="seven">
@@ -9,24 +113,15 @@ export default function Footer(props) {
                     Sign up for our newsletter
                 </p>
                 <div id="seven_socials_div">
-                    <a
-                        id="seven_facebook_a"
-                        target="_blank"
-                        href="https://facebook.com/cloakanddaggercle"
-                    >
-                        <img id="seven_facebook" src="img/CD_Facebook.png" />
+                    <a id="seven_facebook_a" target="_blank" href="https://facebook.com/cloakanddaggercle">
+                        <img id="seven_facebook" className='socials_1' src="img/CD_Facebook.png" />
                     </a>
-
-                    <a
-                        id="seven_instagram_a"
-                        target="_blank"
-                        href="https://instagram.com/cloakanddaggercle"
-                    >
-                        <img id="seven_instagram" src="img/CD_Instagram.png" />
+                    <a id="seven_instagram_a" target="_blank" href="https://instagram.com/cloakanddaggercle" >
+                        <img id="seven_instagram" className='socials_1' src="img/CD_Instagram.png" />
                     </a>
                 </div>
                 <div className="klaviyo-form-RBSHP6"></div>
             </div>
-  </div>
+        </div>
     );
 }
--- src/components/home/Prologue.js
+++ src/components/home/Prologue.js
@@ -1,25 +1,42 @@
 import { React } from 'react';
 
 export default function Prologue(props) {
-    if (window.innerWidth < 992) {
-        
-    
-    if (window.innerWidth < 678) {
-    return (
-        <div id='main_text' className='my-5'>
-            <img className='prologue_text_3' src='../img/prologue.png' />
-            <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
-            <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
-        </div>
-    );
-    }
-    return (
-        <div id='main_text' className='my-5'>
-            <img className='prologue_text_2' src='../img/prologue.png' />
-            <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
-            <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
-        </div>
-    );
+    if (window.innerWidth < 1500) {
+        if (window.innerWidth < 1200) {
+            if (window.innerWidth < 992) {
+                if (window.innerWidth < 768) {
+                    return (
+                        <div id='main_text' className='my-5'>
+                            <img className='prologue_text_5' src='../img/prologue.png' />
+                            <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
+                            <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
+                        </div>
+                    );
+                }
+                return (
+                    <div id='main_text' className='my-5'>
+                        <img className='prologue_text_4' src='../img/prologue.png' />
+                        <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
+                        <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
+                    </div>
+                );
+            }
+
+            return (
+                <div id='main_text' className='my-5'>
+                    <img className='prologue_text_3' src='../img/prologue.png' />
+                    <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
+                    <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
+                </div>
+            );
+        }
+        return (
+            <div id='main_text' className='my-5'>
+                <img className='prologue_text_2' src='../img/prologue.png' />
+                <p className='font-color-1 mt-5 big_text'>A cocktail lounge with a focus on hospitality and craft located next to Lincoln Park in the historic Tremont neighborhood of Cleveland.</p>
+                <p className='font-color-2 smaller_text mt-5'>Tues - Sun <span className='font-color-1'>4pm - 11pm</span></p>
+            </div>
+        );
     }
     return (
         <div id='main_text' className='my-5'>
