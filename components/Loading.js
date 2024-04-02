42954b31af3c1c5e5e2242b173d035688d48ac84
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-04-01 22:06:09 -0400

    Delete components directory

--- components/Loading.js
+++
@@ -1,9 +0,0 @@
-import { React } from 'react';
-
-export default function Loading(props) {
-    return (
-        <div id='loading_div' className=''>
-            <h1>Loading...</h1>
-        </div>
-    );
-}
--- components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import { React } from 'react';
-
-export default function NotFound(props) {
-    return (
-        <div id='home_div' className=''>
-            <h1>Not Found</h1>
-        </div>
-    );
-}
