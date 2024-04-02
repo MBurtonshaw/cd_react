8e7a66a24e6a610fe3471d6a35e09ff78b71915e
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-04-01 22:17:57 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,52 @@
 import { React } from 'react';
 
 export default function Header(props) {
+    function color_coded() {
+        if (window.location.pathname === '/menu') {
+            return (
+                <div className='d-flex justify-content-end nav_bar'>
+                    <a className='px-2 nav_item nonchalant font-color-2' href='/menu'>MENU</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' target='_blank' href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>TAKEOUT</a>
+                </div>
+            );
+        }
+        if (window.location.pathname === '/videos') {
+            return (
+                <div className='d-flex justify-content-end nav_bar'>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/menu'>MENU</a>
+                    <a className='px-2 nav_item nonchalant font-color-2' href='/videos'>VIDEOS</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' target='_blank' href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>TAKEOUT</a>
+                </div>
+            )
+        }
+        if (window.location.pathname === '/gallery') {
+            return (
+                <div className='d-flex justify-content-end nav_bar'>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/menu'>MENU</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
+                    <a className='px-2 nav_item nonchalant font-color-2' href='/gallery'>GALLERY</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' target='_blank' href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>TAKEOUT</a>
+                </div>
+            )
+        }
+        if (window.location.pathname === '/contact') {
+            return (
+                <div className='d-flex justify-content-end nav_bar'>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/menu'>MENU</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
+                    <a className='px-2 nav_item nonchalant font-color-2' href='/contact'>CONTACT</a>
+                    <a className='px-2 nav_item nonchalant font-color-1' target='_blank' href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>TAKEOUT</a>
+                </div>
+            )
+        }
+    }
     if (window.innerWidth < 992) {
         return (
             <div id='header_div'>
@@ -20,6 +66,7 @@ export default function Header(props) {
                                 <li><a className="dropdown-item" href='/contact'>Contact</a></li>
                                 <li><a className="dropdown-item" href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>Takeout</a></li>
                             </ul>
+
                         </div>
 
 
@@ -41,13 +88,7 @@ export default function Header(props) {
                     <a className='' href='/'><img className='nav_img p-1' src='../img/CD_Logo_Horizontal.png' /></a>
                 </div>
                 <div className='col'>
-                    <div className='d-flex justify-content-end nav_bar'>
-                        <a className='px-2 nav_item nonchalant font-color-1' href='/menu'>MENU</a>
-                        <a className='px-2 nav_item nonchalant font-color-1' href='/videos'>VIDEOS</a>
-                        <a className='px-2 nav_item nonchalant font-color-1' href='/gallery'>GALLERY</a>
-                        <a className='px-2 nav_item nonchalant font-color-1' href='/contact'>CONTACT</a>
-                        <a className='px-2 nav_item nonchalant font-color-1' target='_blank' href='https://app.upserve.com/s/cloak-and-dagger-cocktail-lounge-lakewood'>TAKEOUT</a>
-                    </div>
+                    {color_coded()}
                 </div>
             </div>
             <div className='address_bar'>
