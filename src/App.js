2860a4819ebdc4b4449ccf0daf17535dad2a7bae
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-04-02 13:49:40 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/home/Home';
-import About from './components/about/About';
 import Gallery from './components/gallery/Gallery';
 import Contact from './components/contact/Contact';
-import Menu from './components/home/Menu';
+import Menu from './components/menu/Menu';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Videos from './components/videos/Videos';
@@ -16,7 +15,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/gallery' element={<Gallery />} />
           <Route path='/menu' element={<Menu />} />
